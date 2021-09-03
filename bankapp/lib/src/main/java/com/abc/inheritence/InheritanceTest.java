116c0d98e8d790047608f3bd0ba87c2124bd18cd
Prasad <prasad.yoaspire@gmail.com>
2021-09-03 16:22:13 +0530

    inheritance added.

---
+++ bankapp/lib/src/main/java/com/abc/inheritence/InheritanceTest.java
@@ -0,0 +1,38 @@
+package com.abc.inheritence;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class InheritanceTest {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("CAP-DB");
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		
+		//create one employee
+		Employee employee = new Employee();
+		//employee.setEmployeeId(111);
+		employee.setName("John");
+		employee.setSalary(5000);
+		em.persist(employee);
+		
+		//create one manager
+		Manager manager = new Manager();
+		//manager.setEmployeeId(222);
+		manager.setName("Trisha");
+		manager.setSalary(8000);
+		manager.setDepartmentName("Sales");
+		em.persist(manager);
+		
+		
+		
+		em.getTransaction().commit();
+		
+		System.out.println("Added one employee and manager to database.");
+		em.close();
+		factory.close();
+	}
+}
